8dd66c5c6b0547ff26fbdb30f63731b6848152a5
Sebastián Suárez <sebastian.suarezr@udea.edu.co>
2023-04-14 18:41:35 -0500

    New endpoints added, some logic moved to mappers and custom exceptions created

--- src/role/schemas.py
+++ src/role/schemas.py
@@ -1,7 +1,8 @@
-from typing import Optional
+from typing import Optional, List
 
 from sqlmodel import SQLModel
-
+from src.employee.models import Employee
+from src.seniority_level.models import SeniorityLevel
 
 class RoleCreate(SQLModel):
     name: str
@@ -11,3 +12,15 @@ class RoleCreate(SQLModel):
 class RoleUpdate(SQLModel):
     name: Optional[str]
     description: Optional[str]
+
+class RoleEmployees(SQLModel):
+    id: int
+    name: str
+    description: str
+    employees: List[Employee]
+
+class RoleSeniorityLevels(SQLModel):
+    id: int
+    name: str
+    description: str
+    seniority_levels: List[SeniorityLevel]
