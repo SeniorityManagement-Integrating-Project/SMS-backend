b64c0de75af2a35c44557a0ad944fe98462eacff
Sebastián Suárez <sebastian.suarezr@udea.edu.co>
2023-04-19 14:43:30 -0500

    skill request exceptions refactor

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from src.employee.exceptions import add_employee_exception_handlers
 from src.role.exceptions import add_role_exception_handlers
 from src.interaction.exceptions import add_interaction_exception_handlers
 from src.skill.exceptions import add_skill_exception_handlers
+from src.skill_validation_request.exceptions import add_request_exception_handlers
 
 
 app = FastAPI()
@@ -24,13 +25,13 @@ def read_root():
 app.include_router(employee.router, prefix="/employee", tags=["Employee"])
 app.include_router(role.router, prefix="/role", tags=["Role"])
 app.include_router(interaction.router, prefix="/interaction", tags=["Interaction"])
-app.include_router(seniority_level.router,
-                   prefix="/seniority_level", tags=["Seniority Level"])
+app.include_router(seniority_level.router, prefix="/seniority_level", tags=["Seniority Level"])
 app.include_router(skill.router, prefix="/skill", tags=["Skill"])
 app.include_router(request.router, prefix="/request", tags=["Request"])
 
 
-add_employee_exception_handlers(app)
 add_role_exception_handlers(app)
-add_interaction_exception_handlers(app)
 add_skill_exception_handlers(app)
+add_request_exception_handlers(app)
+add_employee_exception_handlers(app)
+add_interaction_exception_handlers(app)
--- src/skill_validation_request/router.py
+++ src/skill_validation_request/router.py
@@ -1,11 +1,18 @@
 from typing import List
 
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status
 
 import src.skill_validation_request.service as request_service
-from src.skill_validation_request.exceptions import RequestNotFound, RequestAlreadyApproved, EmployeeWithoutRequests
-from src.skill_validation_request.models import SkillValidationRequest, SkillValidationRequestComment
-from src.skill_validation_request.schemas import RequestCreate, RequestUpdate, RequestComment, RequestCommentCreate
+from src.skill_validation_request.models import (
+    SkillValidationRequest,
+    SkillValidationRequestComment,
+)
+from src.skill_validation_request.schemas import (
+    RequestCreate,
+    RequestUpdate,
+    RequestComment,
+    RequestCommentCreate,
+)
 
 router = APIRouter()
 
@@ -17,51 +24,41 @@ def get_all() -> List[SkillValidationRequest]:
 
 @router.get("/{skill_validation_request_id}")
 def get(skill_validation_request_id: int) -> SkillValidationRequest:
-    try:
-        request = request_service.get(skill_validation_request_id)
-        return request
-    except RequestNotFound as exc:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(exc)) from exc
+    return request_service.get(skill_validation_request_id)
 
 
 @router.get("/employee/{employee_id}")
 def get_by_employee(employee_id: int) -> List[SkillValidationRequest]:
-    try:
-        return request_service.get_by_employee(employee_id)
-    except EmployeeWithoutRequests as exc:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(exc)) from exc
+    return request_service.get_by_employee(employee_id)
 
 
 @router.post("/{employee_id}/{skill_id}")
 def create(employee_id: int, skill_id: int, request: RequestCreate) -> SkillValidationRequest:
-    try:
-        return request_service.create(employee_id, skill_id, request)
-    except RequestAlreadyApproved as exc:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(exc)) from exc
+    return request_service.create(employee_id, skill_id, request)
 
 
-@router.patch("/{skill_validation_request_id}", status_code=status.HTTP_200_OK, response_model=SkillValidationRequest)
+@router.patch(
+    "/{skill_validation_request_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=SkillValidationRequest,
+)
 def update(skill_validation_request_id: int, request: RequestUpdate):
-    try:
-        return request_service.update(skill_validation_request_id, request)
-    except RequestNotFound as exc:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(exc)) from exc
+    return request_service.update(skill_validation_request_id, request)
 
 
-@router.get("/comments/{skill_validation_request_id}",
-            description="Broke if any of the attributes of the request is null")
+@router.get(
+    "/comments/{skill_validation_request_id}",
+    description="Broke if any of the attributes of the request is null",
+)
 def get_with_comments(skill_validation_request_id: int) -> RequestComment:
-    try:
-        return request_service.get_with_comments(skill_validation_request_id)
-    except RequestNotFound as exc:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(exc)) from exc
+    return request_service.get_with_comments(skill_validation_request_id)
 
 
-@router.post("/comments/{skill_validation_request_id}",
-             description="Not working yet. Cannot create a comment after query a request. Maybe because the session.")
-def create_comment(skill_validation_request_id: int,
-                   request_comment: RequestCommentCreate) -> SkillValidationRequestComment:
-    try:
-        return request_service.create_comment(skill_validation_request_id, request_comment)
-    except RequestNotFound as exc:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(exc)) from exc
+@router.post(
+    "/comments/{skill_validation_request_id}",
+    description="Not working yet. Cannot create a comment after query a request. Maybe because the session.",
+)
+def create_comment(
+    skill_validation_request_id: int, request_comment: RequestCommentCreate
+) -> SkillValidationRequestComment:
+    return request_service.create_comment(skill_validation_request_id, request_comment)
